34c0ea3d5d023749fdf1c5c11337d7f03729dcd2
WillianSavio <Williansavio98@gmail.com>
2026-02-26 19:49:33 -0300

    Desafio_funcoes_correcao

--- Desafio_funcoes_python.py
+++ Desafio_funcoes_python.py
@@ -2,23 +2,30 @@ def soma(n1, n2):
     return n1 + n2
 
 resultado_soma = soma(12, 13)
-print(resultado_soma)
+print(f"Soma: {resultado_soma}")
 
 def subtrair(n1, n2):
     return n1 - n2
 
 resultado_subtrair = subtrair(8, 3)
-print(resultado_subtrair)
+print(f"Subtração: {resultado_subtrair}")
 
 
 def divisao (n1, n2):
     return n1 / n2
 
 resultado_divisao = divisao(450, 3)
-print(resultado_divisao)
+print(f"Divisão: {resultado_divisao}")
 
 def multiplicacao(n1, n2):
     return n1 * n2
 
 resultado_multiplicacao = multiplicacao(2, 1)
-print(resultado_multiplicacao)
+print(f"Multiplicação: {resultado_multiplicacao}")
+
+def porcentagem(valor, percentual):
+    return (valor * percentual) / 100
+
+resultado_porcentual = porcentagem(1000, 10)
+print(f"Porcentagem: {resultado_porcentual}")
+print(f"10% de 100 é {resultado_porcentual}")
