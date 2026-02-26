c7ba3082b13eeecaaf2b61b60f0981973ed08e97
WillianSavio <Williansavio98@gmail.com>
2026-02-25 21:43:53 -0300

    Funcoes-python

---
+++ Desafio_funcoes_python.py
@@ -0,0 +1,24 @@
+def soma(n1, n2):
+    return n1 + n2
+
+resultado_soma = soma(12, 13)
+print(resultado_soma)
+
+def subtrair(n1, n2):
+    return n1 - n2
+
+resultado_subtrair = subtrair(8, 3)
+print(resultado_subtrair)
+
+
+def divisao (n1, n2):
+    return n1 / n2
+
+resultado_divisao = divisao(450, 3)
+print(resultado_divisao)
+
+def multiplicacao(n1, n2):
+    return n1 * n2
+
+resultado_multiplicacao = multiplicacao(2, 1)
+print(resultado_multiplicacao)
---
+++ Funcoes.py
@@ -0,0 +1,25 @@
+# COMO CRIAR FUNÇOES EM PYTHON
+
+## ESTRUTURA DE UMA FUNÇÃO ##
+def nome_da_funcao():  
+    # AQUI FICA O MEU BLOCO DE CÓDIGO
+    # AQUI TAMBÉM
+    # E ASSIM VAI
+    print("Olá galera!!")
+
+nome_da_funcao()
+
+
+#FUNÇÃO COM PARAMETRO NOME
+def inicio_conversa(nome):
+    print(f"Falaaaa, {nome}! TEM SERVIDOR DHCP AIIII???!!!")
+
+inicio_conversa("Willian")
+
+
+def soma(n1, n2):
+    return n1 + n2
+
+resultado = soma(3, 18)
+
+print(resultado)
